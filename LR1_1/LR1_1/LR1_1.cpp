7131061ec4b68e556916a3b09e935906f64c9a4b
Виктория Якубова <s106597@guu.ru>
2021-06-24 23:29:44 +0300

    Lab 1 is done

---
+++ LR1_1/LR1_1/LR1_1.cpp
@@ -0,0 +1,26 @@
+﻿//Написать программу, которая заменяет все четные числа в целочисленном статическом массиве на 0. (массив из 10 элементов)
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    const int M = 10;
+    int A[M];
+    cout << "Введите элементы массива:" << endl;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> A[i];                                //заполнение массива;
+    }
+    for (int i = 0; i < M; i++)
+    {
+        if (A[i] % 2 == 0)  //проверка условия на четность элемента и замена на 0;
+            A[i] = 0;
+    }
+    cout << "Полученный массив:" << endl;
+    for (int i = 0; i < M; i++)
+        cout << setw(2) << A[i] << " ";                       // вывод массива;
+    system("pause");
+    return 0;
+}
+
---
+++ LR1_3/LR1_3/LR1_3.cpp
@@ -0,0 +1,35 @@
+﻿//Расчет определителя матрицы, матрица размером 3 на 3. Ввод чисел построчно.Числа только целые.
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	const int m = 3, n = 3;
+	int d = 0;
+	int a[m][n];
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Ввод строки" << i << endl;
+		for (int j = 0; j < n; j++)		//построчное заполнение матрицы;
+		{
+			cout << "Ввод элемента" << j << " ";
+			cin >> a[i][j];
+		}
+	}
+	cout << "Исходная матрица:" << endl;
+	for (int i = 0; i < m; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < n; j++)		//вывод матрицы;
+		{
+			cout << setw(10) << setprecision(2) << a[i][j];
+		}
+	}
+	cout << endl;
+	d = a[0][0] * a[1][1] * a[2][2] + a[2][0] * a[0][1] * a[1][2] + a[1][0] * a[2][1] * a[0][2] - a[2][0] * a[1][1] * a[0][2] - a[0][0] * a[2][1] * a[1][2] - a[1][0] * a[0][1] * a[2][2]; //расчет определителя по формуле;
+	cout << "Определитель матрицы=" << d;
+	system("pause");
+	return 0;
+}
+
